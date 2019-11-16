3f1e28d6d3b129137130ae42288563b03c42866d
Antonin Herrero <herrero.antonin@gmail.com>
2019-11-16 19:18:38 +0100

    (feat) Sample class BeatPulser

---
+++ Assets/Scripts/BeatPulser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * BeatPulser is an exemple class to show the use of BeatManager.
+ */
+public class BeatPulser : MonoBehaviour
+{
+    private float lastBeat = 0.0f;
+
+    void Update()
+    {
+        if (BeatManager.songPosition > lastBeat + BeatManager.period) {
+            Debug.Log("boum");
+            lastBeat += BeatManager.period;
+        }
+    }
+}
