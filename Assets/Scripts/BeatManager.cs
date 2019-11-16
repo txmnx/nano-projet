d6be5523c5e1bd01b5c8d0a7c17e22d4fa391e8b
Antonin Herrero <herrero.antonin@gmail.com>
2019-11-16 19:04:31 +0100

    (feat) Added BeatManager class

---
+++ Assets/Scripts/BeatManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * The BeatManager class provides methods and properties to be used by everything that needs to be synced to the beat.
+ * TODO:
+ *      Since there is no music to be synced to at the moment we use Time.time as a timer
+ *      but we'll have to change it to AudioSettings.dspTime to be more precise.
+ */
+public class BeatManager : MonoBehaviour
+{
+    public static int bpm = 60;
+
+    public static int period {
+        get { return 60 / bpm; }
+    }
+    
+    public static float songPosition {
+        get { return Time.time; }
+    }
+}
