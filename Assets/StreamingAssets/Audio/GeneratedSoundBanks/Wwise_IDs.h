9b72e00f6c1b70664197b11b5d7dcb728c9bd187
TerrySound <56301490+TerrySound@users.noreply.github.com>
2019-11-30 13:23:37 +0100

    (ADD) Music event callback Add on sample scene gameobject Music_Start

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,19 +13,41 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MUSIC_R0_INIT = 2178242372U;
         static const AkUniqueID SON_TEMOIN = 1502450958U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID WWISE_SNB = 1286985910U;
+        static const AkUniqueID SNB_MAIN = 845961116U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID CHARACTER_1 = 1204435050U;
+        static const AkUniqueID CHARACTER_2 = 1204435049U;
+        static const AkUniqueID COMMON = 2395677314U;
+        static const AkUniqueID DYNAMIC = 2223635772U;
+        static const AkUniqueID IMPACT = 3257506471U;
+        static const AkUniqueID IN_GAME = 2967546505U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID REVERB_MENU = 1031316333U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID STATIC = 1409504247U;
+        static const AkUniqueID TITLE = 3705726509U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VO = 1534528548U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB_ENVIRONMENT = 2846107099U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
