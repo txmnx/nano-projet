adfd4f5cedb2df41f7a31b3bf3676035e5764872
TerrySound <56301490+TerrySound@users.noreply.github.com>
2019-12-03 15:42:10 +0100

    (UPDATE) Soundbank Wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,76 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMB_MAIN_START = 1418672556U;
+        static const AkUniqueID AMB_MENU_START = 3958906054U;
         static const AkUniqueID MUSIC_R0_INIT = 2178242372U;
+        static const AkUniqueID PLAY_MAIN_MUSIC = 414863633U;
+        static const AkUniqueID SFX_AMERICAN_DEATH = 2506831292U;
+        static const AkUniqueID SFX_AMERICAN_FENTE = 3508642320U;
+        static const AkUniqueID SFX_AMERICAN_LASER_IMPACT_SUCCES = 119557261U;
+        static const AkUniqueID SFX_AMERICAN_LASER_SHOOT = 4275657479U;
+        static const AkUniqueID SFX_AMERICAN_PARTICLES = 3421765259U;
+        static const AkUniqueID SFX_AMERICAN_SHIELD = 1159243881U;
+        static const AkUniqueID SFX_AMERICAN_SPECIALATTACK = 3672859679U;
+        static const AkUniqueID SFX_COMMON_ELECTRIC_SPARKS = 2927774105U;
+        static const AkUniqueID SFX_COMMON_OIL = 2534836963U;
+        static const AkUniqueID SFX_COMMON_PROJECTILE_WOOSH = 1636459093U;
+        static const AkUniqueID SFX_INPUTPHASE_IN = 427260706U;
+        static const AkUniqueID SFX_INPUTPHASE_OUT = 1142057693U;
+        static const AkUniqueID SFX_JAPAN_DEATH = 2398750736U;
+        static const AkUniqueID SFX_JAPAN_FENTE = 217610004U;
+        static const AkUniqueID SFX_JAPAN_LASER_IMPACT_SUCCES = 1314745705U;
+        static const AkUniqueID SFX_JAPAN_LASER_SHOOT = 394728051U;
+        static const AkUniqueID SFX_JAPAN_PARTICLES = 2168208239U;
+        static const AkUniqueID SFX_JAPAN_SHIELD = 3056811045U;
+        static const AkUniqueID SFX_JAPAN_SPECIALATTACK = 1941465131U;
+        static const AkUniqueID SFX_TITLE_FIGHT = 2335777874U;
+        static const AkUniqueID SFX_TITLE_TIMER = 3290104963U;
+        static const AkUniqueID SFX_TITLE_WIN = 1985058514U;
         static const AkUniqueID SON_TEMOIN = 1502450958U;
+        static const AkUniqueID VO_AMERICAN_DEATH = 390301034U;
+        static const AkUniqueID VO_AMERICAN_WIN = 957164294U;
+        static const AkUniqueID VO_JAPAN_DEATH = 978479414U;
+        static const AkUniqueID VO_JAPAN_WIN = 2169528442U;
+        static const AkUniqueID VO_SPEAKER_AMERICAN_WIN = 2188929916U;
+        static const AkUniqueID VO_SPEAKER_AMERICAN_WIN_FIGHT = 1547089927U;
+        static const AkUniqueID VO_SPEAKER_JAPAN_WIN = 3061101440U;
+        static const AkUniqueID VO_SPEAKER_JAPAN_WIN_FIGHT = 1444082811U;
+        static const AkUniqueID VO_SPEAKER_ROUND_1 = 3174962419U;
+        static const AkUniqueID VO_SPEAKER_ROUND_2 = 3174962416U;
+        static const AkUniqueID VO_SPEAKER_START_FIGHT = 3797693660U;
+        static const AkUniqueID VO_SPEAKER_ULTIMEROUND = 3097949019U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICTOPLAY
+        {
+            static const AkUniqueID GROUP = 3012001959U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IDLE = 1874288895U;
+                static const AkUniqueID R0_INIT = 274558428U;
+                static const AkUniqueID ST_R1_LEADJP = 2263923035U;
+                static const AkUniqueID ST_R1_LEADUS = 2482032081U;
+                static const AkUniqueID ST_R2_DRAW = 2043216656U;
+                static const AkUniqueID ST_R2_LEADJP = 3955955234U;
+                static const AkUniqueID ST_R2_LEADUS = 4274730056U;
+                static const AkUniqueID ST_ROUNDWINJP = 2323118955U;
+                static const AkUniqueID ST_ROUNDWINUS = 2004344129U;
+                static const AkUniqueID ST_WINJP = 3028319585U;
+                static const AkUniqueID ST_WINUS = 3179318095U;
+            } // namespace STATE
+        } // namespace MUSICTOPLAY
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_AMERICAN_HEALTH = 1818309602U;
+        static const AkUniqueID RTPC_JAPAN_HEALTH = 3984115470U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -26,22 +93,22 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMERICAN = 2437018863U;
         static const AkUniqueID CHARACTER = 436743010U;
-        static const AkUniqueID CHARACTER_1 = 1204435050U;
-        static const AkUniqueID CHARACTER_2 = 1204435049U;
         static const AkUniqueID COMMON = 2395677314U;
-        static const AkUniqueID DYNAMIC = 2223635772U;
         static const AkUniqueID IMPACT = 3257506471U;
         static const AkUniqueID IN_GAME = 2967546505U;
+        static const AkUniqueID JAPAN = 532012563U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MENU = 2607556080U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID REVERB_MENU = 1031316333U;
         static const AkUniqueID SFX = 393239870U;
-        static const AkUniqueID STATIC = 1409504247U;
+        static const AkUniqueID SPEAKER = 2748973500U;
         static const AkUniqueID TITLE = 3705726509U;
         static const AkUniqueID UI = 1551306167U;
         static const AkUniqueID VO = 1534528548U;
+        static const AkUniqueID VO_CHARACTER = 482648852U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
