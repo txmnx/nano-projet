d5d9d52842183509e7b37f15cede5d81315a4e64
boyaki <bpa.gauthier@gmail.com>
2019-12-09 15:27:34 +0100

    Interface fonctionel
    Fuck this shit

---
+++ Assets/Scripts/MainMenuBenoit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBenoit : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text theText;
+    public GameObject controlMenuUI;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.red; //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.white; //Or however you do your color
+    }
+
+    public void LoadVersus()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OptionMenu()
+    { 
+    }
+
+    public void ControlMenu()
+    {
+        controlMenuUI.SetActive(true);
+    }
+  public void QuitControlMenu()
+    {
+        controlMenuUI.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        //Application.Quit;
+    }
+}
+//Isfocus
+//IShighlight
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject controlMenuUI;
+    public GameObject pauseMenuUI;
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }   
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+    public void ControlMenu()
+    {
+        controlMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void QuitControlMenu()
+    {
+        controlMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+    public void OptionMenu()
+    {
+
+    }
+}
