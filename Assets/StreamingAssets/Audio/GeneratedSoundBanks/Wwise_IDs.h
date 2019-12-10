a35d05593d0af76f3437baf76410ab649aff33fb
PamplemousseOui <56346804+PamplemousseOui@users.noreply.github.com>
2019-12-10 16:16:22 +0100

    [UPDATE] SNB

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,13 +17,11 @@ namespace AK
         static const AkUniqueID AMB_MENU_START = 3958906054U;
         static const AkUniqueID FOLEY_ROBOT_DAMAGE = 1606116385U;
         static const AkUniqueID FOLEY_ROBOT_DEATH = 1218823660U;
-        static const AkUniqueID FOLEY_ROBOT_DESTROYED = 1640025331U;
         static const AkUniqueID FOLEY_ROBOT_FENTE = 1796177216U;
         static const AkUniqueID FOLEY_ROBOT_GUARD = 3951945397U;
         static const AkUniqueID FOLEY_ROBOT_IDDLE = 1531385158U;
         static const AkUniqueID FOLEY_ROBOT_LASER_DOOR_CLOSE = 3924484359U;
         static const AkUniqueID FOLEY_ROBOT_LASER_DOOR_OPEN = 2167064369U;
-        static const AkUniqueID FOLEY_ROBOT_LASER_IMPACT_SUCCES = 2263646269U;
         static const AkUniqueID FOLEY_ROBOT_LOSE_ARM = 248344350U;
         static const AkUniqueID FOLEY_ROBOT_SHIELD = 2768415929U;
         static const AkUniqueID FOLEY_ROBOT_SPECIALATTACK = 2142273903U;
@@ -38,6 +36,7 @@ namespace AK
         static const AkUniqueID SFX_INPUTPHASE_IN = 427260706U;
         static const AkUniqueID SFX_INPUTPHASE_OUT = 1142057693U;
         static const AkUniqueID SFX_ROBOT_DEATH = 591816388U;
+        static const AkUniqueID SFX_ROBOT_EXPLOSION = 3716377771U;
         static const AkUniqueID SFX_ROBOT_FENTE = 1708613752U;
         static const AkUniqueID SFX_ROBOT_LASER_IMPACT_REFLECT = 2599727010U;
         static const AkUniqueID SFX_ROBOT_LASER_IMPACT_SUCCES = 3580743349U;
