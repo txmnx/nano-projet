efbad8e40f0c843b89fe659c26088f1b19df5795
Max Panic <themaxpanic40@gmail.com>
2019-12-10 14:15:49 +0100

    IntroManager (WIP)

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -11,13 +11,14 @@ public class IntroManager : MonoBehaviour
     public InputTranslator it;
     public FightManager fm;
     public BeatManager bm;
+    public AkEvent ae;
 
     public float counter;
 
     // Start is called before the first frame update
     void Start()
     {
-        counter = 100.0f;
+        counter = 4.0f;
     }
 
     // Update is called once per frame
@@ -26,18 +27,20 @@ public class IntroManager : MonoBehaviour
         counter -= Time.deltaTime;
         if(counter <= 0)
         {
+            
             it.enabled = true;
             gm.enabled = true;
             fm.enabled = true;
             bm.enabled = true;
             p1.enabled = true;
             p2.enabled = true;
+            ae.useCallbacks = true;
             it.customAwake();
             fm.customAwake();
             bm.customAwake();
             it.customStart();
             fm.customStart();
-            gm.customStart();          
+            gm.customStart();
             p1.customStart();
             p2.customStart();
         }
