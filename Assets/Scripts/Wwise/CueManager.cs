9397b21a74bc696d14816ff8f69bb26e607cb2de
PamplemousseOui <56346804+PamplemousseOui@users.noreply.github.com>
2019-12-11 14:37:56 +0100

    [ADD] CueManager [UPDATE] SNB

---
+++ Assets/Scripts/Wwise/CueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueManager : MonoBehaviour
+{
+
+    public int cueCounter = 0;
+    public bool isCounting = true;
+
+    public void CueFunction()
+    {
+        if (isCounting)
+        {
+            cueCounter++;
+
+            switch (cueCounter)
+            {
+                case 1:         //Début de la musique d'intro
+                    
+                    break;
+
+                case 2:         //Flag annonce round
+
+                    break;
+
+                case 3:         //Flag lancement fight
+
+                    break;
+
+                case 4:         //Début d'outro
+
+                    break;
+
+                case 5:         //Fin d'outro / Initialisation prochain round
+
+                    break;
+            }
+        }
+    }
+
+    public void StartCueCounting()
+    {
+        isCounting = true;
+    }
+
+    public void StopCueCounting()
+    {
+        isCounting = false;
+    }
+}
