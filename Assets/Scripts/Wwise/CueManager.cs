d56816c46395009647cf74e2320a8d291fc50108
PamplemousseOui <56346804+PamplemousseOui@users.noreply.github.com>
2019-12-11 16:37:39 +0100

    [UPDATE] Cue manager

--- Assets/Scripts/Wwise/CueManager.cs
+++ Assets/Scripts/Wwise/CueManager.cs
@@ -6,7 +6,7 @@ public class CueManager : MonoBehaviour
 {
 
     public int cueCounter = 0;
-    public bool isCounting = true;
+    public bool isCounting = false;
 
     public void CueFunction()
     {
@@ -17,22 +17,27 @@ public class CueManager : MonoBehaviour
             switch (cueCounter)
             {
                 case 1:         //Début de la musique d'intro
+                    //Functions to call
                     
                     break;
 
                 case 2:         //Flag annonce round
+                    //Functions to call
 
                     break;
 
                 case 3:         //Flag lancement fight
+                    //Functions to call
 
                     break;
 
                 case 4:         //Début d'outro
+                    //Functions to call
 
                     break;
 
                 case 5:         //Fin d'outro / Initialisation prochain round
+                    //Functions to call
 
                     break;
             }
