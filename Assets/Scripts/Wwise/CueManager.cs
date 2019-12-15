d28203c390dcad59abeacebbfbca714c37a1234f
Max Panic <themaxpanic40@gmail.com>
2019-12-15 17:17:37 +0100

    Text Ui integration and animation

--- Assets/Scripts/Wwise/CueManager.cs
+++ Assets/Scripts/Wwise/CueManager.cs
@@ -9,7 +9,11 @@ public class CueManager : MonoBehaviour
     public int cueCounter = 0;
     public static bool isCounting = false;
     public bool isBeatDetected = false;
-
+    public GameObject round1Sprite;
+    public GameObject round2Sprite;
+    public GameObject fightSprite;
+    public GameObject victory1Sprite;
+    public GameObject victory2Sprite;
 
     public MatchManager matchManager;
 
@@ -26,18 +30,32 @@ public class CueManager : MonoBehaviour
                     break;
 
                 case 2:         //Flag annonce round
+                    if (matchManager.players[0].wins == 0 && matchManager.players[1].wins == 0)
+                        round1Sprite.GetComponent<Animator>().Play("AnimRound", 0, 0);
+                    else if ((matchManager.players[0].wins == 0 && matchManager.players[1].wins == 1) || (matchManager.players[0].wins == 1 && matchManager.players[1].wins == 0))
+                        round2Sprite.GetComponent<Animator>().Play("AnimRound", 0, 0);
                     //Functions to call
 
                     break;
 
                 case 3:         //Flag lancement fight
                     //Functions to call
+                    fightSprite.GetComponent<Animator>().Play("AnimFight");
                     StopCueCounting();
                     break;
 
                 case 4:         //Début d'outro
                     //Functions to call
                     InputTranslator.sequence = Sequence.IDLE;
+
+                    if (matchManager.matchIsEnd)
+                    {
+                        if (matchManager.players[0].wins > matchManager.players[1].wins)
+                            victory1Sprite.GetComponent<Animator>().Play("AnimVictory", 0, 0);
+                        else
+                            victory2Sprite.GetComponent<Animator>().Play("AnimVictory", 0, 0);
+                    }
+                   
                     isBeatDetected = false;
                     matchManager.onRoundEnd();
                     if (matchManager.matchIsEnd)
